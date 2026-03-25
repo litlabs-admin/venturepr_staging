c3ba0061a22576027767fde8b117b84da7f16dd8
litlabs-admin <engineering@litlabs.agency>
2026-03-25 23:42:33 +0530

    updates

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,26 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import { BrowserRouter } from "react-router-dom";
+import {
+  UnifyIntentClient,
+  UnifyIntentProvider,
+} from '@unifygtm/intent-react';
+
+const writeKey = 'wk_SYSpUbBb_8jcxAiRjQfBPZdqThAiHE5vdygBJw7ZG';
+
+const config = {
+  autoPage: true,
+  autoIdentify: false,
+};
+
+const intentClient = new UnifyIntentClient(writeKey, config);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <UnifyIntentProvider intentClient={intentClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </UnifyIntentProvider>
   </React.StrictMode>
 );
